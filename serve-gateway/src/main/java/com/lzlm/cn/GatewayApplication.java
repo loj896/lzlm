12d08bb9564ec4b496820100820b84ecdbb5f799
liuhai <liuhai116163@163.com>
2019-12-05 16:46:51 +0800

    添加服务网关模块

---
+++ serve-gateway/src/main/java/com/lzlm/cn/GatewayApplication.java
@@ -0,0 +1,34 @@
+package com.lzlm.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/***
+ *                    .::::. 
+ *                  .::::::::. 
+ *                 :::::::::::        @author liuhai
+ *             ..:::::::::::'         @date 2019-12-05 16:35
+ *           '::::::::::::'           @description 服务网关
+ *             .:::::::::: 
+ *        '::::::::::::::.. 
+ *             ..::::::::::::. 
+ *           ``:::::::::::::::: 
+ *            ::::``:::::::::'        .:::. 
+ *           ::::'   ':::::'       .::::::::. 
+ *         .::::'      ::::     .:::::::'::::. 
+ *        .:::'       :::::  .:::::::::' ':::::. 
+ *       .::'        :::::.:::::::::'      ':::::. 
+ *      .::'         ::::::::::::::'         ``::::. 
+ *  ...:::           ::::::::::::'              ``::. 
+ * ```` ':.          ':::::::::'                  ::::.. 
+ *                    '.:::::'                    ':'````.. 
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
