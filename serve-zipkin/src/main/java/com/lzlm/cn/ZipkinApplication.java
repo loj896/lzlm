6be09dc6feb71c466c794a4f96ab0efe9ec84238
liuhai <liuhai116163@163.com>
2019-12-06 16:45:32 +0800

    添加链路跟踪模块serve-zipkin

---
+++ serve-zipkin/src/main/java/com/lzlm/cn/ZipkinApplication.java
@@ -0,0 +1,36 @@
+package com.lzlm.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/***
+ *                    .::::. 
+ *                  .::::::::. 
+ *                 :::::::::::        @author liuhai
+ *             ..:::::::::::'         @date 2019-12-06 14:01
+ *           '::::::::::::'           @description
+ *             .:::::::::: 
+ *        '::::::::::::::.. 
+ *             ..::::::::::::. 
+ *           ``:::::::::::::::: 
+ *            ::::``:::::::::'        .:::. 
+ *           ::::'   ':::::'       .::::::::. 
+ *         .::::'      ::::     .:::::::'::::. 
+ *        .:::'       :::::  .:::::::::' ':::::. 
+ *       .::'        :::::.:::::::::'      ':::::. 
+ *      .::'         ::::::::::::::'         ``::::. 
+ *  ...:::           ::::::::::::'              ``::. 
+ * ```` ':.          ':::::::::'                  ::::.. 
+ *                    '.:::::'                    ':'````.. 
+ */
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
