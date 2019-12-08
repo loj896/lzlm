4e67ea63a51a652ce8fbc35d24dad04a81c36e49
liuha <loj897637)(>
2019-12-08 22:09:47 +0800

    更新说明文档

---
+++ serve-common/src/main/java/com/lzlm/cn/dao/auth/AuthClientDetailsMapper.java
@@ -0,0 +1,10 @@
+package com.lzlm.cn.dao.auth;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lzlm.cn.model.auth.AuthClientDetails;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AuthClientDetailsMapper extends BaseMapper<AuthClientDetails> {
+
+}
---
+++ serve-common/src/main/java/com/lzlm/cn/model/auth/AuthClientDetails.java
@@ -0,0 +1,119 @@
+package com.lzlm.cn.model.auth;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("oauth_client_details")
+public class AuthClientDetails {
+
+    @TableId
+    private String clientId;
+
+    private String resourceIds;
+
+    private String clientSecret;
+
+    private String scope;
+
+    private String authorizedGrantTypes;
+
+    private String webServerRedirectUri;
+
+    private String authorities;
+
+    private Integer accessTokenValidity;
+
+    private Integer refreshTokenValidity;
+
+    private String additionalInformation;
+
+    private String autoapprove;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId == null ? null : clientId.trim();
+    }
+
+    public String getResourceIds() {
+        return resourceIds;
+    }
+
+    public void setResourceIds(String resourceIds) {
+        this.resourceIds = resourceIds == null ? null : resourceIds.trim();
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret == null ? null : clientSecret.trim();
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope == null ? null : scope.trim();
+    }
+
+    public String getAuthorizedGrantTypes() {
+        return authorizedGrantTypes;
+    }
+
+    public void setAuthorizedGrantTypes(String authorizedGrantTypes) {
+        this.authorizedGrantTypes = authorizedGrantTypes == null ? null : authorizedGrantTypes.trim();
+    }
+
+    public String getWebServerRedirectUri() {
+        return webServerRedirectUri;
+    }
+
+    public void setWebServerRedirectUri(String webServerRedirectUri) {
+        this.webServerRedirectUri = webServerRedirectUri == null ? null : webServerRedirectUri.trim();
+    }
+
+    public String getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(String authorities) {
+        this.authorities = authorities == null ? null : authorities.trim();
+    }
+
+    public Integer getAccessTokenValidity() {
+        return accessTokenValidity;
+    }
+
+    public void setAccessTokenValidity(Integer accessTokenValidity) {
+        this.accessTokenValidity = accessTokenValidity;
+    }
+
+    public Integer getRefreshTokenValidity() {
+        return refreshTokenValidity;
+    }
+
+    public void setRefreshTokenValidity(Integer refreshTokenValidity) {
+        this.refreshTokenValidity = refreshTokenValidity;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation == null ? null : additionalInformation.trim();
+    }
+
+    public String getAutoapprove() {
+        return autoapprove;
+    }
+
+    public void setAutoapprove(String autoapprove) {
+        this.autoapprove = autoapprove == null ? null : autoapprove.trim();
+    }
+}
